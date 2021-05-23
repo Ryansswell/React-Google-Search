324e3624759c54ebc43549feb41f8df3b0696892
Ryan Anderson <ryan0425@msn.com>
2021-05-23 12:53:28 -0700

    SearchForm implements Section

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,23 +1,21 @@
+import Section from "../Section";
+
 function SearchForm() {
     return (
-        <div classname="card">
-            <div classname="card-body">
-                <h2 classname="card-title" style={{ fontSize: "1.3rem" }}>
-                    Book Search
-                    </h2>
-                <form className="d-flex flex-column">
-                    <div className="form-group">
-                        <label htmlFor="search">Book</label>
-                        <input type="text" className="form-control" id="search" placeholder="Enter Book" />
-                    </div>
-                    <button className="btn btn-primary ml-auto px-5">Search</button>
-                </form>
-            </div>
-        </div>
 
+        <Section title="Book Search">.
+            <form className="d-flex flex-column">
+                <div className="form-group">
+                    <label htmlFor="search">Book</label>
+                    <input type="text" className="form-control" id="search" placeholder="Enter Book" />
+                </div>
+                <button className="btn btn-primary ml-auto px-5">Search</button>
+            </form>
+        </Section>
 
     );
 }
+
 
 
 
