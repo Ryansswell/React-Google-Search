3be1d05a16bb863565116736a396d91200d60a12
Ryan Anderson <ryan0425@msn.com>
2021-05-23 12:07:50 -0700

    Add Wrapper to SearchPage

--- client/src/components/SearchPage/index.js
+++ client/src/components/SearchPage/index.js
@@ -1,15 +1,15 @@
 import BookList from "../BookList";
 import Hero from "../Hero";
 import SearchForm from "../SearchForm";
-
+import Wrapper from "../Wrapper";
 
 function SearchPage() {
     return (
-        <>
+        <Wrapper>
             <Hero />
             <SearchForm />
             <BookList />
-        </>
+        </Wrapper>
     );
 }
 
