afb430d108730dc263d1a5f0b8f48de0063ac4c0
Ryan Anderson <ryan0425@msn.com>
2021-05-23 16:39:04 -0700

    updated task breakdown

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -6,9 +6,7 @@ function BookList() {
     return (
         <Section title="Results">
             {books.map(book => <BookCard key={book.id} book={book} />)}
-            <BookCard book={books[0]} />
-            <BookCard book={books[1]} />
-            <BookCard book={books[2]} />
+
         </Section>
 
     );
