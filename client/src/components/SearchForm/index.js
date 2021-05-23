13f55599d2cd7c54750919c182e59114466b5ac3
Ryan Anderson <ryan0425@msn.com>
2021-05-23 12:31:31 -0700

    Add content and style to search form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,7 +1,24 @@
 function SearchForm() {
-    return (<h1> Search Form </h1>
+    return (
+        <div classname="card">
+            <div classname="card-body">
+                <h2 classname="card-title" style={{ fontSize: "1.3rem" }}>
+                    Book Search
+                    </h2>
+                <form className="d-flex flex-column">
+                    <div className="form-group">
+                        <label htmlFor="search">Book</label>
+                        <input type="text" className="form-control" id="search" placeholder="Enter Book" />
+                    </div>
+                    <button className="btn btn-primary ml-auto px-5">Search</button>
+                </form>
+            </div>
+        </div>
+
 
     );
 }
+
+
 
 export default SearchForm;
