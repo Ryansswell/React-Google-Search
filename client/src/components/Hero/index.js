3424954a9d63af6a91e2df4e739722013f0c527e
Ryan Anderson <ryan0425@msn.com>
2021-05-23 12:06:05 -0700

    Add content and style to Hero

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,5 +1,12 @@
 function Hero() {
-    return <div>DA Hero</div>;
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">React Google Books Search</h1>
+                <p class="lead">Search for and Save Books of Interest</p>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
