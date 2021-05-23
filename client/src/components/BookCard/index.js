40c15adc7b944ba0747430002e039a06f95813e0
Ryan Anderson <ryan0425@msn.com>
2021-05-23 16:32:09 -0700

    Render static book list on SearchPage

--- client/src/components/BookCard/index.js
+++ client/src/components/BookCard/index.js
@@ -1,7 +1,34 @@
+function BookCard({ book }) {
+    const { title, authors, description, imageLinks, infoLink } = book.volumeInfo;
+    return (
+        <div className="card mb-3">
+            <div className="row no-gutters">
+                <div className="col-md-3">
+                    <img src={imageLinks.thumbnail}
+                        alt={`thumbnail of ${title} `}
+                    />
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{authors.join(", ")}</p>
+                        <p className="card-text"> {description} </p>
+                        <div className="d-flex justify-content-end">
+                            <a className="btn btn-info mr-3"
+                                href={infoLink}
+                                rel="noopener noreferrer"
+                                target="_blank"
+                            >
+                                View
+                                </a>
+                            <button className="btn btn-success">Save</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-
-function BookCard() {
-    return <div>Book Card item</div>;
+    );
 }
 
 export default BookCard;
--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,16 +1,17 @@
 import BookCard from "../BookCard";
 import Section from "../Section";
+import { items as books } from "../../fakeGoogleBooks.json";
 
 function BookList() {
     return (
         <Section title="Results">
-            <Section>
-                <BookCard />
-                <BookCard />
-                <BookCard />
-            </Section>
-            <Section>
-                );
-                }
+            {books.map(book => <BookCard key={book.id} book={book} />)}
+            <BookCard book={books[0]} />
+            <BookCard book={books[1]} />
+            <BookCard book={books[2]} />
+        </Section>
+
+    );
+}
 
 export default BookList;
